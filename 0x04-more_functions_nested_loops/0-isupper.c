ca827c4ec1f821cae81efd1c058f11e95b56cb82
Beahilu Adugna <behailuadugna0@gmail.com>
2022-06-09 06:34:32 -0700

    fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isupper - checks for uppercase
- * @c: character to checked for case
- *
- * Return: 1 if uppercase, 0 if not uppercase
+ * _isupper - checks for uppercase character
+ * @letter: the character to be tracked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-
-int _isupper(int c)
+int _isupper(int letter)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	return (0);
+	return (letter >= 'A' && letter <= 'Z');
 }
