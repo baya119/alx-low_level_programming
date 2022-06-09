35090a857398448616fbc74ced60536728d854b7
Beahilu Adugna <behailuadugna0@gmail.com>
2022-06-09 06:35:52 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isdigit - checks for digithood
- * @c: character to checked for digit membership
- *
- * Return: 1 if digit, 0 if not digit
+ * _isdigit - checks for a digit (0 through 9)
+ * @num: int to be checked
+ * Return: 1 if num is a digit, 0 otherwise
  */
-
-int _isdigit(int c)
+int _isdigit(int num)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
+	return (num >= '0' && num <= '9');
 }
